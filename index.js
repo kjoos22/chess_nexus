7090beef419170c1e727b8155555ff2ff9e3f34c
kjoos22 <kjoos22@gmail.com>
2021-09-25 15:25:31 -0400

    beginning of building out board render

---
+++ index.js
@@ -0,0 +1,3 @@
+const game = new playChess()
+
+game.drawBoard()
--- src/playChess.js
+++ src/playChess.js
@@ -2,6 +2,17 @@ class playChess {
     
     constructor() {
         this.canvas = document.getElementById("gameBoard")
+        this.ctx = this.canvas.getContext("2d")
+    }
+
+    drawBoard() {
+        var board = new Image()
+        board.src = "src/assets/board squares/square brown dark_svg.svg"
+        board.addEventListener('load', e =>  {
+            this.ctx.drawImage(board, 0, 0, 100, 100)
+        })
+        
+
     }
 
 }
