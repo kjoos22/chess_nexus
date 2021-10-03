5c6b0f2feb2de721c1cc85ab556c08aacc8d4bdc
kjoos22 <kjoos22@gmail.com>
2021-10-03 13:54:15 -0400

    refactored PlayChess constructor to use GameState class

--- src/playChess.js
+++ src/playChess.js
@@ -4,30 +4,11 @@ class PlayChess {
         this.canvas = document.getElementById("gameBoard")
         this.ctx = this.canvas.getContext("2d")
         this.lightSquare = "src/assets/board squares/square brown light.svg"
-        this.darkSquare = "src/assets/board squares/square brown dark.svg"      
+        this.darkSquare = "src/assets/board squares/square brown dark.svg"
+        
+        this.gameState = new GameState()
 
-
-        this.boardCoords = [
-            'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8',
-            'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8',
-            'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8',
-            'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8',
-            'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8',
-            'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8',
-            'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8',
-            'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'
-        ]
-        this.squares = {
-            a1: {x: 0, y: 700},
-            a2: {x: 0, y: 600},
-            a3: {x: 0, y: 500},
-            a4: {x: 0, y: 400},
-            a5: {x: 0, y: 300},
-            a6: {x: 0, y: 200},
-            a7: {x: 0, y: 100},
-            a8: {x: 0, y: 0},
-            h1: {x1: 0, x2: 100, y1: 700, y2:700}
-        }
+        
     }
 
     drawBoard() {
