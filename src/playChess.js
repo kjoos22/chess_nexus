6ddf2ea19d1b74061d29ab017588300239a0e562
kjoos22 <kjoos22@gmail.com>
2021-10-10 15:42:48 -0400

    completed initial rendering of white powerpieces

--- src/playChess.js
+++ src/playChess.js
@@ -40,11 +40,40 @@ class PlayChess {
     }
 
     drawPieces() {
+
         let wRook = new Image()
-        wRook.src = "src/assets/SVG No Shadow/w_rook_svg_NoShadow.svg"
+        wRook.src = "src/assets/pieces/w_rook.svg"
         wRook.addEventListener("load", e => {
             this.ctx.drawImage(wRook, 10, 710, 80, 80)
+            this.ctx.drawImage(wRook, 710, 710, 80, 80)
         })
+
+        let wKnight = new Image()
+        wKnight.src = "src/assets/pieces/w_knight.svg"
+        wKnight.addEventListener("load", e => {
+            this.ctx.drawImage(wKnight, 110, 710, 80, 80)
+            this.ctx.drawImage(wKnight, 610, 710, 80, 80)
+        })
+
+        let wBishop = new Image()
+        wBishop.src = "src/assets/pieces/w_bishop.svg"
+        wBishop.addEventListener("load", e => {
+            this.ctx.drawImage(wBishop, 210, 710, 80, 80)
+            this.ctx.drawImage(wBishop, 510, 710, 80, 80)
+        })
+
+        let wQueen = new Image()
+        wQueen.src = "src/assets/pieces/w_queen.svg"
+        wQueen.addEventListener("load", e => {
+            this.ctx.drawImage(wQueen, 310, 710, 80, 80)
+        })
+
+        let qKing = new Image()
+        qKing.src = "src/assets/pieces/w_king.svg"
+        qKing.addEventListener("load", e => {
+            this.ctx.drawImage(qKing, 410, 710, 80, 80)
+        })
+
     }
         
 
